df8b6c8b177353c3832f404671204bcaf51877a6
khandelwalnaman579 <khandelwalnaman579@gmail.com>
2025-09-19 19:21:24 +0530

    use datatypes in js

---
+++ 01basics/02datatypes.js
@@ -0,0 +1,25 @@
+"use strict";//treat all as newer version of javascript
+//alert(3+3);//provide error--- we are using nodeJs not browser
+console.log(3
+    +
+    3);//never use in this way -- code should be readable
+console.log("Naman Khandelwal");
+
+let name = "Naman";
+let age = 21;
+let isApproved = false;
+
+//number => 2 to the power 53
+//bigint => larger than 2 to the power 53
+//string => '', "", ``
+//boolean => true/false
+//undefined => value not assigned
+//null => empty value
+//symbol => unique identifier
+
+//object => key-value pairs
+console.log(typeof "Naman");
+console.log(typeof age);
+console.log(typeof isApproved);
+console.log(typeof null);
+console.log(typeof undefined);
